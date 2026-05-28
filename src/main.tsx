16982062753cbadad4a823c88c742ed2014fd50a
umanglabs <umanglabs@gmail.com>
2026-05-28 11:22:09 +0530

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -3,11 +3,7 @@ import { createRoot } from 'react-dom/client';
 import App from './App.tsx';
 import { AuthProvider } from './contexts/AuthContext';
 import './index.css';
-if ('serviceWorker' in navigator) {
-  window.addEventListener('load', () => {
-    navigator.serviceWorker.register('/sw.js');
-  });
-}
+
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <AuthProvider>
